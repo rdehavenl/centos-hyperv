e00d607e1cc39a634cb2ce968a1cfccbcd89bd5a
Richard DeHaven <rdehavenl@eentorweb.com>
2018-05-25 09:48:46 +0800

    template and vagrant user add

---
+++ packer/boot/scripts/setup-vagrant.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -x
+set -e
+
+vagrant_home=/home/vagrant
+vagrant_pubkey_url="https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub"
+
+mkdir ${vagrant_home}/.ssh
+
+curl -o ${vagrant_home}/.ssh/authorized_keys ${vagrant_pubkey_url}
+
+chown -R vagrant:vagrant ${vagrant_home}/.ssh
+chmod -R 0775 ${vagrant_home}/.ssh
+chmod 0644  ${vagrant_home}/.ssh/authorized_keys
